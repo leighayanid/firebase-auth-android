d0d9e857d09d9baf41dae4dcc0eeb5f2e987e327
Leigh R. Dinaya <leighdinaya04@gmail.com>
2016-06-20 18:02:05 +0800

    Added utility for checking network state

--- app/src/main/java/sample/com/firebaseauth/utils/Utils.java
+++ app/src/main/java/sample/com/firebaseauth/utils/Utils.java
@@ -1,12 +1,23 @@
 package sample.com.firebaseauth.utils;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 /**
  * Created by user on 6/20/2016.
  */
 public class Utils {
 
-    public boolean isConnected(){
+    public boolean isConnectionAvailable(Context context){
+        ConnectivityManager mgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = mgr.getActiveNetworkInfo();
 
-        return true;
+        boolean isConnected = info.isConnectedOrConnecting();
+        if (isConnected){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
